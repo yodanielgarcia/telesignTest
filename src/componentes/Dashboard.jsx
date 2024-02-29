620c0e61dae35eb2424a62b9228624bcb1ff7b94
daniel.garca <daniel.garca@nordstrom.com>
2024-02-29 14:46:26 -0500

    show info

--- src/componentes/Dashboard.jsx
+++ src/componentes/Dashboard.jsx
@@ -8,9 +8,10 @@ export default function Dashboard() {
   useEffect(() => {
     services.getRequest().then((movie) => setMovies(movie));
   }, []);
-  const getGenere = () => {
-    //services.getGenere().then((movie) => setMovies(movie));
-    return moviesResult.genreId
+  const getGenere = (movie) => {
+    //Here call to endpoint that not exist currenly
+    //services.getGenere().then((movie) => setMovies(movie)); 
+    return movie.genre_ids
   };
   return (
     <article style={{ color: "white" }}>
@@ -25,7 +26,7 @@ export default function Dashboard() {
                   height={80}
                 />
                 <h3>{movie.title}</h3>
-                <h4>{getGenere()}</h4>
+                <h4>genere id {getGenere(movie)}</h4>
               </Grid>
             );
           })}
